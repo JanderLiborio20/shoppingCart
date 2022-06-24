030db5be1eabae2348c3ebb17e99511db8589059
jander liborio <jandernunes14@gmail.com>
2022-06-24 16:56:01 -0400

    colocado o useREf

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
+import { createContext, ReactNode, useContext, useEffect, useRef, useState } from 'react';
 import { toast } from 'react-toastify';
 import { api } from '../services/api';
 import { Product, Stock } from '../types';
@@ -32,6 +32,22 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     return [];
   });
 
+  const prevCartRef = useRef<Product[]>();
+
+  useEffect(() => {
+    prevCartRef.current = cart;
+  })
+
+  const cartPreviousValue = prevCartRef.current ?? cart;
+  
+  useEffect(() => {
+    if(cartPreviousValue !== cart){
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(cart))
+    }
+  }, [cart, cartPreviousValue])
+
+
+
   const addProduct = async (productId: number) => {
     try {
       const updated = [...cart];
@@ -61,8 +77,6 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
       }
 
       setCart(updated)
-      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updated))
-
     } catch {
       toast.error('Erro na adição do produto');
     }
